ab113191a1e871fb9acbd14a16c8692f52e978db
dontchoke <dandanplayz2010@gmail.com>
2026-09-09 15:15:09 -0500

    Customize arena introduction

--- game.py
+++ game.py
@@ -1,16 +1,16 @@
 from goblin import Goblin
 
 
-ARENA_NAME = "The Iron Circle"
+ARENA_NAME = "The Oracle Forest"
 
 
 def main():
     """Open the arena and introduce its first opponent."""
     print(f"Welcome to {ARENA_NAME}!")
     print("༼ ᓄºل͟º ༽ᓄ   ᕦ(ò_óˇ)ᕤ")
-    print("The gates are opening...")
+    print("oooopen the gates.................................................. or die")
 
-    goblin = Goblin("Gribble")
+    goblin = Goblin("goog")
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
     print("But no hero has answered the call... yet.")
