74c75d501a3942809c6b9b560af4eaba8ce97c16
dontchoke <dandanplayz2010@gmail.com>
2026-09-14 15:19:22 -0500

    build hero class

--- game.py
+++ game.py
@@ -1,7 +1,9 @@
 from goblin import Goblin
+from hero import Hero
 
 
 ARENA_NAME = "The Oracle Forest"
+
 
 
 def main():
@@ -12,11 +14,24 @@ def main():
 
     goblin2 = Goblin("gooblin")
     goblin = Goblin("goog")
+    hero = Hero("guy")
     
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
     print(f"{goblin2.name} enters the arena with {goblin2.health} health.")
-    print("But no hero has answered the call... yet.")
+    print(f"the hero: {hero.name} enters the arena with {hero.health} health.")
 
+    heroFirstAttack = hero.attack()
+    print(f"{hero.name} attacks {goblin.name}, landing {heroFirstAttack} damage!")
+    goblin.take_damage(heroFirstAttack)
+ 
+
+    if(goblin.is_alive):
+        anotherAttack = hero.attack()
+        print(f"{hero.name} attacks {goblin.name}, landing {anotherAttack} damage!")
+        goblin.take_damage(anotherAttack)
+    
+
+    
 
 if __name__ == "__main__":
     main()
--- hero.py
+++ hero.py
@@ -1,5 +1,32 @@
-class Hero:
-    """The hero blueprint will be implemented later in the project."""
-    
+import random
 
-    pass
+class Hero:
+    
+    #Battle cry: Add a battle_cry() method that prints a custom line.
+    def battle_cry():
+        print("aaaaaaaaaaaaaaaaaaa")
+
+    def __init__(self, name):
+        # Create the Hero's attributes here
+        print("hi")
+    
+        self.name = name
+        self.health = 100
+        self.attack_power = 14
+
+    def attack(self):
+    # Return a random value from 1 through this Hero's attack power.
+        Hero.battle_cry()
+        return random.randint(1, self.attack_power)
+       
+
+    def take_damage(self, damage):
+        # Subtract damage, but do not allow health to fall below 0.
+        if self.health <= 0:
+            self.health -= damage
+
+    def is_alive(self):
+        # Return a Boolean based on this Hero's health.
+        return (self.health <= 0)
+
+
